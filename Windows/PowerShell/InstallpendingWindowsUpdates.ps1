083cbd12e476ddb95aa979d0a55367030ab8ad0b
Joerg Hochwald <joerg.hochwald@enabling.technology>
2022-05-30 12:34:53 +0200

    Add the Scopr to the Nuget Package Provider installation

--- Windows/PowerShell/InstallpendingWindowsUpdates.ps1
+++ Windows/PowerShell/InstallpendingWindowsUpdates.ps1
@@ -58,10 +58,12 @@ begin
    if (-not (Get-PackageProvider @paramGetPackageProvider))
    {
       $paramInstallPackageProvider = @{
-         Name          = 'NuGet'
-         Force         = $true
-         ErrorAction   = $SCT
-         WarningAction = $SCT
+         Name            = 'NuGet'
+         RequiredVersion = '2.8.5.201'
+         scope           = 'AllUsers'
+         Force           = $true
+         ErrorAction     = $SCT
+         WarningAction   = $SCT
       }
       $null = (Install-PackageProvider @paramInstallPackageProvider)
    }
