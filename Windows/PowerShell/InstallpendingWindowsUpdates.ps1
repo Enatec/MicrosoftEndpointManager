b2f01562c4381eee1ee64df8d16c0b58150ea7c1
Joerg Hochwald <joerg.hochwald@enabling.technology>
2022-05-30 10:32:18 +0200

    Add quotes around the version to fix #4

--- Windows/PowerShell/InstallpendingWindowsUpdates.ps1
+++ Windows/PowerShell/InstallpendingWindowsUpdates.ps1
@@ -25,10 +25,10 @@
 param
 (
    [Parameter(ValueFromPipeline = $true,
-              ValueFromPipelineByPropertyName = $true)]
+      ValueFromPipelineByPropertyName = $true)]
    [ValidateNotNullOrEmpty()]
    [version]
-   $PSWindowsUpdateVersion = 2.2.0.3
+   $PSWindowsUpdateVersion = '2.2.0.3'
 )
 
 begin
@@ -36,14 +36,14 @@ begin
    #region Global
    $SCT = 'SilentlyContinue'
    #endregion Global
-   
+
    #region NuGetPackageProvider
    if (-not (Get-PackageProvider -Name NuGet -ErrorAction $SCT -WarningAction $SCT))
    {
       $null = Install-PackageProvider -Name NuGet -Force
    }
    #endregion NuGetPackageProvider
-   
+
    #region MakePSGalleryTrusted
    if (Get-PSRepository -Name PSGallery | Where-Object -FilterScript {
          $PSItem.InstallationPolicy -ne 'Trusted'
@@ -52,7 +52,7 @@ begin
       $null = (Set-PSRepository -Name PSGallery -InstallationPolicy Trusted)
    }
    #endregion MakePSGalleryTrusted
-   
+
    #region CheckPSWindowsUpdate
    # Check if we have the latest version installed
    if (-not (Get-Module -Name PSWindowsUpdate -ListAvailable -ErrorAction $SCT -WarningAction $SCT | Sort-Object -Descending -Property Version | Select-Object -First 1 | Where-Object -FilterScript {
@@ -63,7 +63,7 @@ begin
       $null = (Install-Module -Name PSWindowsUpdate -Force -MinimumVersion $PSWindowsUpdateVersion -Repository PSGallery -AllowClobber:$true)
    }
    #endregion CheckPSWindowsUpdate
-   
+
    #region ImportPSWindowsUpdate
    $null = (Import-Module -Name PSWindowsUpdate -Force -DisableNameChecking -NoClobber -MinimumVersion $PSWindowsUpdateVersion -ErrorAction $SCT -WarningAction $SCT)
    #endregion ImportPSWindowsUpdate
@@ -75,7 +75,7 @@ process
    try
    {
       Write-Output -InputObject 'Install all available drivers'
-      
+
       $null = (Get-WindowsUpdate -Install -MicrosoftUpdate -UpdateType Driver -IgnoreUserInput -AcceptAll -IgnoreReboot -ForceInstall -ComputerName $env:ComputerName -Confirm:$false -ErrorAction $SCT -WarningAction $SCT)
    }
    catch
@@ -83,12 +83,12 @@ process
       Write-Verbose -Message 'Install all available drivers issue'
    }
    #endregion InstallPendingDrivers
-   
+
    #region InstallPendingUpdates
    try
    {
       Write-Output -InputObject 'Install all available updates, except SilverLight'
-      
+
       $null = (Get-WindowsUpdate -Install -MicrosoftUpdate -NotKBArticleID KB4481252 -IgnoreUserInput -AcceptAll -IgnoreReboot -ForceDownload -ComputerName $env:ComputerName -Confirm:$false -ErrorAction $SCT -WarningAction $SCT)
    }
    catch
@@ -101,18 +101,18 @@ process
 end
 {
    $needReboot = ((Get-WURebootStatus -ComputerName $env:ComputerName -Silent -ErrorAction $SCT -WarningAction $SCT).RebootRequired)
-   
+
    if ($needReboot)
    {
       Write-Output -InputObject 'Reboot required'
-      
+
       # Set return code 3010. As long as this happens during device ESP, the computer will automatically reboot at the end of device ESP.
       exit 3010
    }
    else
    {
       Write-Output -InputObject 'No reboot required'
-      
+
       exit 0
    }
 }
