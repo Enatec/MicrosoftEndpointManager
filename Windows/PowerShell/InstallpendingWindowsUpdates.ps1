859b557747f30d2bde981824802219d933cfa85e
Joerg Hochwald <joerg.hochwald@enabling.technology>
2022-05-30 12:37:13 +0200

    Cosmetic change ;-)

--- Windows/PowerShell/InstallpendingWindowsUpdates.ps1
+++ Windows/PowerShell/InstallpendingWindowsUpdates.ps1
@@ -60,7 +60,7 @@ begin
       $paramInstallPackageProvider = @{
          Name            = 'NuGet'
          RequiredVersion = '2.8.5.201'
-         scope           = 'AllUsers'
+         Scope           = 'AllUsers'
          Force           = $true
          ErrorAction     = $SCT
          WarningAction   = $SCT
