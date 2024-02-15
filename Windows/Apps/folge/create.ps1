08090741b412d3e504c4ccb41751168b5f77e04a
Joerg Hochwald <joeg.hochwald@enabling.technology>
2024-02-15 14:35:46 +0100

    Version update (Folge)

--- Windows/Apps/folge/create.ps1
+++ Windows/Apps/folge/create.ps1
@@ -1,2 +1,2 @@
-$Installer = 'Folge-1.16.1.exe'
+$Installer = 'Folge-1.17.0.exe'
 $null = (& "$env:ChocolateyInstall\bin\intunewinapputil.exe" -c ('{0}\Source' -f $PSScriptRoot) -s $Installer -o ('{0}\Output' -f $PSScriptRoot) -q)
