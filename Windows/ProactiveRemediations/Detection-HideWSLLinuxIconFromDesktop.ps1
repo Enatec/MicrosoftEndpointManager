f7e5a7ac6f65b751c64b560093e0eaae87a43a15
joerg@hochwald.net <joerg@hochwald.net>
2024-07-01 19:52:56 +0200

    ✨ Code reformated and minor changes

--- Windows/ProactiveRemediations/Detection-HideWSLLinuxIconFromDesktop.ps1
+++ Windows/ProactiveRemediations/Detection-HideWSLLinuxIconFromDesktop.ps1
@@ -2,22 +2,22 @@
 
 $RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel'
 
-try 
+try
 {
-  if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
-  {
-    Exit 1
-  }
+   if (!(Test-Path -LiteralPath $RegPath -ErrorAction SilentlyContinue))
+   {
+      Exit 1
+   }
 
-  # 1 = OFF (Hide) / Replace with 0 to turn it back on  
-  if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name '{B2B4A4D1-2754-4140-A2EB-9A76D9D7CDC6}' -ErrorAction SilentlyContinue) -eq 1)) 
-  {
-    Exit 1
-  } 
+   # 1 = OFF (Hide) / Replace with 0 to turn it back on
+   if (!((Get-ItemPropertyValue -LiteralPath $RegPath -Name '{B2B4A4D1-2754-4140-A2EB-9A76D9D7CDC6}' -ErrorAction SilentlyContinue) -eq 1))
+   {
+      Exit 1
+   }
 }
-catch 
+catch
 {
-  Exit 1
+   Exit 1
 }
 
 Exit 0
