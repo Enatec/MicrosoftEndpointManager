bd153408c0a3cbbbecf6c6072eb7fc326fa50a7e
Joerg Hochwald <jhochwald@users.noreply.github.com>
2024-08-27 00:35:54 +0200

    Add: Windows Autopatch
    Windows Autopatch: Auto-remediation

---
+++ Windows/ProactiveRemediations/Remediation-AutoPatch.ps1
@@ -0,0 +1,61 @@
+﻿# Remediation-AutoPatch
+# https://techcommunity.microsoft.com/t5/windows-it-pro-blog/windows-autopatch-auto-remediation-with-powershell-scripts/ba-p/4228854
+
+$TranscriptPath = ('{0}\Microsoft\IntuneManagementExtension\Logs' -f $env:ProgramData)
+$TranscriptName = 'Remediation-AutoPatch.log'
+$null = (New-Item -Path $TranscriptPath -ItemType Directory -Force -Confirm:$false -ErrorAction SilentlyContinue)
+
+# Stopping orphaned transcripts
+try
+{
+   $null = (Stop-Transcript -ErrorAction SilentlyContinue)
+}
+catch
+{
+   Write-Verbose -Message $_
+}
+
+# Start transcript
+$null = (Start-Transcript -Path ('{0}\{1}' -f $TranscriptPath, $TranscriptName) -Append -Force -Confirm:$false -ErrorAction Stop)
+
+# Initialize the array
+[PsObject[]]$regkeys = @()
+
+# Populate the array with each object
+$regkeys += [PsObject]@{
+   Name = 'DoNotConnectToWindowsUpdateInternetLocations'
+   Path = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\'
+}
+$regkeys += [PsObject]@{
+   Name = 'DisableWindowsUpdateAccess'
+   Path = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\'
+}
+$regkeys += [PsObject]@{
+   Name = 'NoAutoUpdate'
+   Path = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\'
+}
+
+foreach ($setting in $regkeys)
+{
+   Write-Output -InputObject ('Checking {0}' -f $setting.Name)
+
+   if ((Get-Item -Path $setting.Path -ErrorAction SilentlyContinue).Property -contains $setting.name)
+   {
+      Write-Output -InputObject ('Remediating {0}' -f $setting.Name)
+
+      $null = (Remove-ItemProperty -Path $setting.Path -Name $($setting.name) -Force -Confirm:$false -ErrorAction SilentlyContinue)
+   }
+   else
+   {
+      Write-Output -InputObject ('{0} was not found' -f $setting)
+   }
+}
+
+try
+{
+   $null = (Stop-Transcript -ErrorAction SilentlyContinue)
+}
+catch
+{
+   Write-Verbose -Message $_
+}
