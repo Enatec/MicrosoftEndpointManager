dc2c8e430766bd2d04d3d2821fae923614dfdb46
Joerg Hochwald <jhochwald@users.noreply.github.com>
2026-05-09 00:09:07 +0200

    Prevent Google Chrome's 4GB AI model from reinstalling itself

---
+++ Windows/ProactiveRemediations/Detection-ChromeGenAILocalFoundationalModelSettings.ps1
@@ -0,0 +1,32 @@
+﻿# Check: Prevent Google Chrome's 4GB AI model from reinstalling itself
+# Detection-ChromeGenAILocalFoundationalModelSettings.ps1
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Google\Chrome'
+
+try
+{
+   $paramTestPath = @{
+      LiteralPath = $RegPath
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!(Test-Path @paramTestPath))
+   {
+      exit 1
+   }
+   
+   $paramGetItemPropertyValue = @{
+      LiteralPath = $RegPath
+      Name        = 'GenAILocalFoundationalModelSettings'
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!((Get-ItemPropertyValue @paramGetItemPropertyValue) -eq 1))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+exit 0
