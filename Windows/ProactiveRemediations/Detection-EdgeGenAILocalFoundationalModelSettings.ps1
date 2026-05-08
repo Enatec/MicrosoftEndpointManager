d2310c80958e1172ca849d291518a18a0d168c4b
Joerg Hochwald <jhochwald@users.noreply.github.com>
2026-05-09 00:10:41 +0200

    Prevent Microsoft Edge's 4GB AI model from reinstalling itself

---
+++ Windows/ProactiveRemediations/Detection-EdgeGenAILocalFoundationalModelSettings.ps1
@@ -0,0 +1,32 @@
+﻿# Check: Prevent Microsoft Edge's 4GB AI model from reinstalling itself
+# Detection-EdgeGenAILocalFoundationalModelSettings.ps1
+
+$RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
+
+try 
+{
+   $paramTestPath = @{
+      LiteralPath = $RegPath
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!(Test-Path @paramTestPath))
+   {
+      exit 1
+   }
+   
+   $paramGetItemPropertyValue = @{
+      LiteralPath = $RegPath
+      Name        = 'GenAILocalFoundationalModelSettings'
+      ErrorAction = 'SilentlyContinue'
+   }
+   if (!((Get-ItemPropertyValue @paramGetItemPropertyValue ) -eq 1))
+   {
+      exit 1
+   }
+}
+catch
+{
+   exit 1
+}
+
+exit 0
